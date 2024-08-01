e1c14ee48d4f524af65ce02bd0308159b72fd367
Luis Miguel <lorozcoh@unal.edu.co>
2024-07-31 22:31:22 -0500

    Create Persona.java
    Elementos básicos clase Persona.

---
+++ src/gestorAplicacion/Usuario/Persona.java
@@ -0,0 +1,11 @@
+package gestorAplicacion.Usuario;
+
+public abstract class Persona {
+
+    protected int cedula;
+    protected String nombre;
+
+    @Override
+    public abstract String toString();
+
+}

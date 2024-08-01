594ac15c43483c4d49a463e0ee55ad9229b20eb6
Luis Miguel <lorozcoh@unal.edu.co>
2024-07-31 22:34:50 -0500

    Update Restaurante.java
    Métodos para la funcionalidad N°2

--- src/gestorAplicacion/Gestion/Restaurante.java
+++ src/gestorAplicacion/Gestion/Restaurante.java
@@ -9,7 +9,7 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Restaurante {
-    //Atributos
+    // Atributos
     public static ArrayList<Cliente> clientes = new ArrayList<Cliente>();
     public static ArrayList<Plato> menu = new ArrayList<Plato>();
     public static ArrayList<Mesa> mesas = new ArrayList<Mesa>();
@@ -25,10 +25,11 @@ public class Restaurante {
     private ArrayList<Ingrediente> bodega = new ArrayList<Ingrediente>();
     private ArrayList<Reserva> reservas = new ArrayList<Reserva>();
 
-    //Constructores
+    // Constructores
     public Restaurante() {
         restaurantesCreados++;
     }
+
     public Restaurante(Ciudad ciudad, Zona zona, boolean zonaVIP) {
         restaurantesCreados++;
         this.ciudad = ciudad;
@@ -36,71 +37,187 @@ public class Restaurante {
         this.zonaVIP = zonaVIP;
     }
 
-    //Métodos
+    // Métodos
     static Scanner consola = new Scanner(System.in);
+
     static String readString() {
         return consola.nextLine();
     }
+
     static int readInt() {
         return Integer.parseInt(consola.nextLine());
     }
+
+    /*
+     * METODO CONFIMAR CLIENTE FUNCIONALIDAD # 2
+     * FUNCIONAMIENTO: recibe como parametro la cedula de el cliente, con este
+     * parametro se busca
+     * dentro del arryList clientes, si existe algun cliente con esta
+     * identificacion. De ser asi
+     * 1. se busca el indice dentro del arraylist que contenta el mismo numero de
+     * cedula(cc) que se le paso como argumento
+     * 2. Con el indice se busca el objeto cliente y se retorna por completo.
+     * En caso de no estar presente se llama al metodo (...) para crear un cliente-
+     */
+
+    public static boolean confirmarCliente(int cc) {
+
+        boolean in = false;
+        Cliente cliente = new Cliente();
+
+        for (Cliente i : clientes) {
+
+            if (i.getCedula() == cc) {
+                in = true;
+                break;
+            }
+        }
+        return in;
+    }
+
+    // Necesario para funcionalidad #2
+
+    public static Cliente crearCliente(int cc) {
+
+        System.out.println("La cédula ingresada es: " + cc);
+        System.out.print("¿Es correcta? (1. Confirmar, 2. Cambiar): ");
+        int opcion = readInt();
+        consola.nextLine();
+
+        if (opcion == 2) {
+            System.out.print("Ingrese la nueva cédula: ");
+            cc = readInt();
+            consola.nextLine();
+        }
+
+        System.out.print("Se necesita el siguiente dato:\nNombre: ");
+        String nombre = consola.nextLine();
+
+        clientes.add(new Cliente(cc, nombre));
+        System.out.println("Cliente agregado exitosamente.");
+
+        return clientes.get(clientes.size() - 1);
+
+    }
+
+    public static void platosOferta(String tipo) {
+
+        switch (tipo) {
+
+            case "Entrada":
+                for (Plato p : menu) {
+                    if (p.getTipo() == "Entrada") {
+                        System.out.println(p);
+                    }
+                }
+
+            case "Fuerte":
+                for (Plato p : menu) {
+                    if (p.getTipo() == "Entrada") {
+                        System.out.println(p);
+                    }
+                }
+
+            case "Bebidas":
+                for (Plato p : menu) {
+                    if (p.getTipo() == "Entrada") {
+                        System.out.println(p);
+                    }
+                }
+
+            case "Postre":
+                for (Plato p : menu) {
+                    if (p.getTipo() == "Entrada") {
+                        System.out.println(p);
+                    }
+                }
+
+            case "Infantil":
+                for (Plato p : menu) {
+                    if (p.getTipo() == "Entrada") {
+                        System.out.println(p);
+                    }
+                }
+        }
+
+    }
+
     public boolean isZonaVIP() {
         return zonaVIP;
     }
+
     public void setZonaVIP(boolean zonaVIP) {
         this.zonaVIP = zonaVIP;
     }
+
     public Zona getZona() {
         return zona;
     }
+
     public void setZona(Zona zona) {
         this.zona = zona;
     }
+
     public Ciudad getCiudad() {
         return ciudad;
     }
+
     public void setCiudad(Ciudad ciudad) {
         this.ciudad = ciudad;
     }
+
     public int getCalificacion() {
         return calificacion;
     }
+
     public void setCalificacion(int calificacion) {
         this.calificacion = calificacion;
     }
+
     public ArrayList<ArrayList<String>> getDisposicion() {
         return disposicion;
     }
+
     public void setDisposicion(ArrayList<ArrayList<String>> disposicion) {
         this.disposicion = disposicion;
     }
+
     public int getCoordX() {
         return coordX;
     }
+
     public void setCoordX(int coordX) {
         this.coordX = coordX;
     }
+
     public int getCoordY() {
         return coordY;
     }
+
     public void setCoordY(int coordY) {
         this.coordY = coordY;
     }
+
     public static ArrayList<Mesa> getMesas() {
         return mesas;
     }
+
     public static void setMesas(ArrayList<Mesa> mesas) {
         Restaurante.mesas = mesas;
     }
+
     public ArrayList<Casilla> getCasillas() {
         return casillas;
     }
+
     public void setCasillas(ArrayList<Casilla> casillas) {
         this.casillas = casillas;
     }
+
     public static ArrayList<Plato> getMenu() {
         return menu;
     }
+
     public static void setMenu(ArrayList<Plato> menu) {
         Restaurante.menu = menu;
     }
@@ -117,7 +234,7 @@ public class Restaurante {
         sb.append('}');
         return sb.toString();
     }
-    //    FUNCIONALIDAD NUMERO 4: agregarSede
-//    Interacción 1: elegirCiudad
+    // FUNCIONALIDAD NUMERO 4: agregarSede
+    // Interacción 1: elegirCiudad
 
 }
